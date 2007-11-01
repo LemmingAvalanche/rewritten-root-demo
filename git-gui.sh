LemmingAvalanche/rewritten-root-demo

commit b9c26e1d79dba37183d0250d0cf6081507671eb3

git-gui: Avoid using bold text in entire gui for some fonts
Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

git-gui.sh
@@ -1648,7 +1648,7 @@ proc apply_config {} {
 		set font [lindex $option 1]
 		if {[catch {
 			foreach {cn cv} $repo_config(gui.$name) {
-				font configure $font $cn $cv
+				font configure $font $cn $cv -weight normal
 			}
 			} err]} {
 			error_popup "Invalid font specified in gui.$name:\n\n$err"

commit 06f1ffc0f8287a131fc2944ff5d5622c9ed9ef29

git-gui: Allow users to set font weights to bold
Previously we allowed users to tweak their font weight to be bold by
setting it manually in their ~/.gitconfig prior to starting git-gui.
This was broken in b9c26e1d79dba37183d0250d0cf6081507671eb3 when
Simon set the font weight to normal by default, overridding whatever
we found from the ~/.gitconfig file.

Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

git-gui.sh
@@ -551,8 +551,15 @@ proc apply_config {} {
 		set name [lindex $option 0]
 		set font [lindex $option 1]
 		if {[catch {
+			set need_weight 1
 			foreach {cn cv} $repo_config(gui.$name) {
-				font configure $font $cn $cv -weight normal
+				if {$cn eq {-weight}} {
+					set need_weight 0
+				}
+				font configure $font $cn $cv
+			}
+			if {$need_weight} {
+				font configure $font -weight normal
 			}
 			} err]} {
 			error_popup [strcat [mc "Invalid font specified in %s:" "gui.$name"] "\n\n$err"]
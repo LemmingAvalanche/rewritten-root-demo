LemmingAvalanche/rewritten-root-demo

commit dfa5329acbf518a48189e1ef2ea777a16f906849

Do not scramble password read from .cvspass
Passwords stored in .cvspass are already scrambled, we do not
want to scramble them twice. Only passwords read from the
command line are scrambled.

This fixes a regression introduced by e51214d (git-cvsimport: add support
for cvs pserver password scrambling., 2009-08-14).

Signed-off-by: Pascal Obry <pascal@obry.net>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

git-cvsimport.perl
@@ -238,7 +238,9 @@ sub conn {
 		}
 		my $rr = ":pserver:$user\@$serv:$port$repo";
 
-		unless ($pass) {
+		if ($pass) {
+			$pass = $self->_scramble($pass);
+		} else {
 			open(H,$ENV{'HOME'}."/.cvspass") and do {
 				# :pserver:cvs@mea.tmt.tele.fi:/cvsroot/zmailer Ah<Z
 				while (<H>) {
@@ -252,8 +254,6 @@ sub conn {
 				}
 			};
 		}
-
-		$pass = $self->_scramble($pass);
 
 		my ($s, $rep);
 		if ($proxyhost) {

commit d8765fb1d820b2a47c76829d1beb229f88e7a6e8

cvs: initialize empty password
If we do not read a password from the command line, and there are no
passwords stored in .cvspass, we have to initialize the password with
just "A".

This fixes a regression introduced by dfa5329a (Do not scramble
password read from .cvspass).

Signed-off-by: Clemens Buchacher <drizzd@aon.at>
Signed-off-by: Junio C Hamano <gitster@pobox.com>

git-cvsimport.perl
@@ -253,6 +253,7 @@ sub conn {
 					}
 				}
 			};
+			$pass = "A" unless $pass;
 		}
 
 		my ($s, $rep);